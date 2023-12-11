b078f93c72774944fac1a6f52bbb1076c3c07885
lordeadroid <rishabhrawat34@gmail.com>
2023-12-11 17:03:13 +0530

    added cors for all domains

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,14 +1,5 @@
-const cors = (req, res, next) => {
-  const origin = req.headers.origin;
-  const allowedOrigins = [
-    'http://localhost:3000',
-    'https://kart-cart.vercel.app/',
-    'https://kart-cart.onrender.com/',
-  ];
-
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+const cors = (_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 };
 
