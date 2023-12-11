edd6954ea72a9830eb5c10087130c1032bbfcee5
lordeadroid <rishabhrawat34@gmail.com>
2023-12-11 13:48:50 +0530

    added another domain that can access the api

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,10 +3,11 @@ const cors = (req, res, next) => {
   const allowedOrigins = [
     'http://localhost:3000',
     'https://kart-cart.vercel.app/',
+    'https://kart-cart.onrender.com/',
   ];
 
   if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
   next();
 };
