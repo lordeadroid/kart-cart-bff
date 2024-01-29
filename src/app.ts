08d307309e81f1fd2ab5d085bd76f1325337b690
lordeadroid <rishabhrawat34@gmail.com>
2024-01-29 19:20:23 +0530

    Feature: express app

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import { Express } from "express";
+import * as express from "express";
+
+const { logRequest } = require("./middlewares/logger");
+const {
+  parseCookie,
+  serveHomePage,
+} = require("./handlers/authentication-handlers");
+const cors = require("./middlewares/cors");
+
+const addMiddleware = (app: Express) => {
+  app.use(logRequest);
+  app.use(parseCookie);
+  app.use(express.json());
+  app.use(cors);
+  app.use(express.urlencoded({ extended: true }));
+};
+
+const createApp = () => {
+  const app: Express = express();
+
+  addMiddleware(app);
+  app.get("/", serveHomePage);
+
+  return app;
+};
+
+export default createApp;
