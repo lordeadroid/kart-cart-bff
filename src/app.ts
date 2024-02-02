0816b61cb7bc38ec42af248eb5ab021e8f2ecf97
lordeadroid <rishabhrawat34@gmail.com>
2024-02-02 15:09:51 +0530

    feature: middleware for handling static pages

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ import * as express from "express";
 import logRequest from "./middlewares/logger";
 import cors from "./middlewares/cors";
 import parseCookie from "./middlewares/cookie-parser";
-import { serveHomePage } from "./api/api-handlers";
 
 const addMiddleware = (app: Express) => {
   app.use(logRequest);
@@ -12,13 +11,13 @@ const addMiddleware = (app: Express) => {
   app.use(express.json());
   app.use(cors);
   app.use(express.urlencoded({ extended: true }));
+  app.use("/", express.static("public"));
 };
 
 const createApp = () => {
   const app: Express = express();
 
   addMiddleware(app);
-  app.get("/", serveHomePage);
 
   return app;
 };
