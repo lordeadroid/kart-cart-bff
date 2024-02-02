8ae49fa033c017c2c3feaa044579465fd3e1c1bb
lordeadroid <rishabhrawat34@gmail.com>
2024-02-02 18:05:16 +0530

    moved ts development to separate branch

--- src/api/api-handlers.ts
+++
@@ -1,7 +0,0 @@
-import { ApiRequest } from "../utils/types";
-
-const serveHomePage: ApiRequest = (_, res) => {
-  res.send("This is the homepage for kart cart bff");
-};
-
-export { serveHomePage };
--- src/middlewares/cors.ts
+++
@@ -1,8 +0,0 @@
-import { ApiRequest } from "../utils/types";
-
-const cors: ApiRequest = (_, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-};
-
-export default cors;
