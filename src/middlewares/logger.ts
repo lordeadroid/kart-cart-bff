96e8584927c5870d760c9ddd55d047d9e99d489c
lordeadroid <rishabhrawat34@gmail.com>
2024-02-02 18:05:16 +0530

    moved ts development to separate branch

--- src/middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import { ApiRequest } from "../utils/types";
-
-const logRequest: ApiRequest = (req, _, next) => {
-  const [TIME] = new Date().toTimeString().split(" ");
-  console.log(`${req.method} ${req.path} @ ${TIME} from ${req.headers.origin}`);
-  next();
-};
-
-export default logRequest;
--- src/utils/constants.ts
+++
@@ -1,7 +0,0 @@
-import { ExcludeId } from "./types";
-
-const defaultPortNumber: string = "8000";
-const findAll: {} = {} as const;
-const excludeId: ExcludeId = { projection: { _id: 0 } };
-
-export { defaultPortNumber, findAll, excludeId };
