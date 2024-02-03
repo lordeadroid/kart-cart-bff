2a9863e1fff888a47356f1df78a6a6e4d3e7215b
lordeadroid <rishabhrawat34@gmail.com>
2024-02-03 18:15:36 +0530

    renamed addMiddleware function to setupMiddleware

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import { Express } from "express";
+import express from "express";
+
+import logRequest from "./middlewares/logger";
+import cors from "./middlewares/cors";
+import parseCookie from "./middlewares/cookie-parser";
+
+const setupMiddlewares = (app: Express) => {
+  app.use(logRequest);
+  app.use(parseCookie);
+  app.use(express.json());
+  app.use(cors);
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/", express.static("public"));
+};
+
+const createApp = () => {
+  const app: Express = express();
+
+  setupMiddlewares(app);
+  return app;
+};
+
+export default createApp;
