16c980a6af0d8266c95908d60dbbb04b1552018c
lordeadroid <rishabhrawat34@gmail.com>
2024-02-03 18:15:47 +0530

    removed js files

--- src/middlewares/logger.js
+++
@@ -1,7 +0,0 @@
-const logRequest = (req, _, next) => {
-  const [TIME] = new Date().toTimeString().split(' ');
-  console.log(`${req.method} ${req.path} @ ${TIME} from ${req.headers.origin}`);
-  next();
-};
-
-module.exports = { logRequest };
