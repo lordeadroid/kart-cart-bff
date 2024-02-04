e58f42c2b04717ddfa31bdd53c660601d2996613
lordeadroid <rishabhrawat34@gmail.com>
2024-02-04 12:02:20 +0530

    added test for homepage

---
+++ test/app.test.ts
@@ -0,0 +1,13 @@
+import createApp from "../src/app";
+import * as request from "supertest";
+
+describe("APP", () => {
+  describe("GET /", () => {
+    it("should send the homepage", async () => {
+      await request(createApp())
+        .get("/")
+        .expect(200)
+        .expect("content-type", /html/);
+    });
+  });
+});
