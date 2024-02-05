1982ef5e0c2d2b1ab26ec0a924616ffdeff63427
lordeadroid <rishabhrawat34@gmail.com>
2024-02-05 19:05:11 +0530

    taking uri from env

--- src/lib/db-client.ts
+++ src/lib/db-client.ts
@@ -1,7 +1,11 @@
 import { MongoClient } from "mongodb";
+import "dotenv/config";
 
-const uri =
-  "mongodb+srv://kart-cart:UG1C1dO3uNEjexuc@cluster0.k5mxgck.mongodb.net/?retryWrites=true&w=majority";
-const dbClient = new MongoClient(uri);
+if (!process.env.URI) {
+  throw new Error("URI is not defined");
+}
+
+const uri: string = process.env.URI;
+const dbClient: MongoClient = new MongoClient(uri);
 
 export default dbClient;
