6147cdcb2c1aba1e3accde3acf1ba3758d926a9f
lordeadroid <rishabhrawat34@gmail.com>
2024-02-05 22:20:47 +0530

    updated requestData signature

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,9 +6,9 @@ type ExcludeId = {
   projection: Id;
 };
 
-type RequestData = {
-  databaseName: string;
-  collectionName: string;
-};
+type RequestData = (
+  databaseName: string,
+  collectionName: string
+) => Promise<Object>;
 
 export { ExcludeId, RequestData };
