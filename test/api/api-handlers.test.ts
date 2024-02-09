d1c7f7766e70012213a80581e6b71d74d1f1d5e2
lordeadroid <rishabhrawat34@gmail.com>
2024-02-09 18:30:12 +0530

    added category endpoint

--- test/api/api-handlers.test.ts
+++ test/api/api-handlers.test.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { describe, jest, test } from "@jest/globals";
 import * as request from "supertest";
 
@@ -17,4 +18,27 @@ describe("/category/men", () => {
       .expect("content-type", /json/)
       .expect(categoryData);
   });
+=======
+import { describe, test } from "@jest/globals";
+
+import createApp from "../../src/app";
+import request from "supertest";
+
+describe("GET /category/men", () => {
+  test("should get data for men's category", async () => {
+    const response = await request(createApp()).get("/category/men");
+
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  // test("should send the homepage", async () => {
+  //   jest.clearAllMocks();
+  //   jest.mock("../../src/lib/request-data");
+  //   (requestData as jest.Mock).mockImplementationOnce(() => {});
+
+  //   const response = await request(createApp()).get("/category/men");
+  //   expect(response.status).toBe(200);
+  // });
+>>>>>>> 39be971 (added category endpoint)
 });
