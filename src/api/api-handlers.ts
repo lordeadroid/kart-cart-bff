c95bc817d7d59d4f98d1299536b5ebb741f5d5ef
lordeadroid <rishabhrawat34@gmail.com>
2024-02-17 20:01:08 +0530

    requestData promise to resolve using await

--- src/api/api-handlers.ts
+++ src/api/api-handlers.ts
@@ -5,14 +5,14 @@ const serveHomePage: ApiRequest = (_, res) => {
   res.send("This is the homepage for kart cart bff");
 };
 
-const serveCategory: ApiRequest = (req, res) => {
+const serveCategory: ApiRequest = async (req, res) => {
   const databaseName = "STORE";
   const collectionName = "PRODUCTS";
   const { productCategory } = req.params;
 
-  requestData(databaseName, collectionName).then((data: any) => {
-    res.json(data[productCategory]);
-  });
+  const data: any = await requestData(databaseName, collectionName);
+  
+  res.json(data[productCategory]);
 };
 
 export { serveHomePage, serveCategory };
