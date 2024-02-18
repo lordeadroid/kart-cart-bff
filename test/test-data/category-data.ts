4e68380b5049983bf8e05aee1b40529293c0def1
lordeadroid <rishabhrawat34@gmail.com>
2024-02-18 18:27:43 +0530

    added mock data for men category

--- test/test-data/category-data.ts
+++ test/test-data/category-data.ts
@@ -1,5 +1,42 @@
-const categoryData = {
-  men: [],
+const mockCategoryData = {
+  men: {
+    m1: {
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqs8ELYS2XT0pMw5mpu7V4bdmuZC12ppA_JBbTZ3NzZOk1tJ7Kf-wp6_y_vxDEG5ngh4k&usqp=CAU",
+        "https://rukminim2.flixcart.com/image/850/1000/jox7xjk0/shirt/f/k/m/m-a8atw143-tommy-hilfiger-original-imafb9yrruqzgj88.jpeg?q=20",
+      ],
+      name: "Shirt",
+      mrp: 1999,
+      price: 999,
+    },
+    m2: {
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqs8ELYS2XT0pMw5mpu7V4bdmuZC12ppA_JBbTZ3NzZOk1tJ7Kf-wp6_y_vxDEG5ngh4k&usqp=CAU",
+        "https://rukminim2.flixcart.com/image/850/1000/jox7xjk0/shirt/f/k/m/m-a8atw143-tommy-hilfiger-original-imafb9yrruqzgj88.jpeg?q=20",
+      ],
+      name: "Shirt",
+      mrp: 1999,
+      price: 999,
+    },
+    m3: {
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqs8ELYS2XT0pMw5mpu7V4bdmuZC12ppA_JBbTZ3NzZOk1tJ7Kf-wp6_y_vxDEG5ngh4k&usqp=CAU",
+        "https://rukminim2.flixcart.com/image/850/1000/jox7xjk0/shirt/f/k/m/m-a8atw143-tommy-hilfiger-original-imafb9yrruqzgj88.jpeg?q=20",
+      ],
+      name: "Shirt",
+      mrp: 1999,
+      price: 999,
+    },
+    m4: {
+      images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqs8ELYS2XT0pMw5mpu7V4bdmuZC12ppA_JBbTZ3NzZOk1tJ7Kf-wp6_y_vxDEG5ngh4k&usqp=CAU",
+        "https://rukminim2.flixcart.com/image/850/1000/jox7xjk0/shirt/f/k/m/m-a8atw143-tommy-hilfiger-original-imafb9yrruqzgj88.jpeg?q=20",
+      ],
+      name: "Shirt",
+      mrp: 1999,
+      price: 999,
+    },
+  },
 };
 
-export default categoryData;
+export default mockCategoryData;
